163c15040ad1cb295af498dc1e5d41fd8f5dbdef
Andrew Miller <anmcmill422@gmail.com>
2020-03-01 13:40:23 -0800

    Added Camera Controller
    Camera now follows the player using simple camera controller script. Making the camera a child did not work due to the rotation method we are using to flip the player's sprite.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	
+	GameObject player; 		  //the players game object
+	Vector3 offset;    		  //offset for camera follow
+
+	// happens first
+	void Awake(){
+		//finds the gameobject with the player tag in the scene and assigns it.
+		player = GameObject.FindGameObjectWithTag ("Player");
+		offset = transform.position - player.transform.position;
+	}
+	// happens every frame
+	void Update () {
+		//remember transform (lowercase) refers to the transform of the game object that this script is attached to
+		transform.position = player.transform.position + offset;
+	}
+}
